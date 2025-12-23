5b5354dc1f9bc6ce6032e0fcf6c89ff4400c600b
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-12-23 17:36:31 +0100

    refactor: sort imports, reformat, etc.

--- home/_dynamic/configurator.py
+++ home/_dynamic/configurator.py
@@ -1,37 +1,27 @@
 #!/usr/bin/env python
 
-import json
 import argparse
-from pathlib import Path
-from typing import Mapping
-from socket import gethostname
+import json
+from collections.abc import Mapping
 from os import getenv
+from pathlib import Path
 from tempfile import gettempdir
 
-
 argument_parser = argparse.ArgumentParser("Generate dynamic chezmoi templates")
-argument_parser.add_argument(
-        "source",
-        type=str,
-        metavar="STRING",
-        help="Name of the setting"
-    )
-argument_parser.add_argument(
-        "target",
-        type=Path,
-        metavar="STRING",
-        help="Path to the output file"
-    )
+argument_parser.add_argument("source", type=str, metavar="STRING", help="Name of the setting")
+argument_parser.add_argument("target", type=Path, metavar="STRING", help="Path to the output file")
 cli_args = argument_parser.parse_args()
 
 
 def on_termux() -> bool:
-    expected_prefix =  "/data/data/com.termux/files/usr"
-    return all((
-        getenv("TERMUX_VERSION"),
-        getenv("PREFIX") == expected_prefix,
-        Path(expected_prefix).is_dir(),
-    ))
+    expected_prefix = "/data/data/com.termux/files/usr"
+    return all(
+        (
+            getenv("TERMUX_VERSION"),
+            getenv("PREFIX") == expected_prefix,
+            Path(expected_prefix).is_dir(),
+        )
+    )
 
 
 def common_data() -> Mapping[str, str]:
@@ -40,24 +30,24 @@ def common_data() -> Mapping[str, str]:
     later to be added to any
     .json configuration file
     """
-    host = gethostname()
     home = Path(getenv("HOME", default=""))
-    username = getenv("USER", default="dotfiles_user")
     downloads_directory = home / "Downloads"
     if on_termux():
         downloads_directory = home / "storage" / "shared" / "Download"
-    result = dict((
-        ("temporary_directory", gettempdir()),
-        ("zdotdir", (home / ".config" / "zdotdir").as_posix()),
-        ("youtube_downloads", (downloads_directory / "yt").as_posix()),
-        ("on_termux", on_termux()),
-        ))
+    result = dict(
+        (
+            ("temporary_directory", gettempdir()),
+            ("zdotdir", (home / ".config" / "zdotdir").as_posix()),
+            ("youtube_downloads", (downloads_directory / "yt").as_posix()),
+            ("on_termux", on_termux()),
+        )
+    )
     return result
 
 
 def main() -> None:
     source_file = Path(cli_args.source).with_suffix(".json")
-    with open(source_file, mode="r", encoding="utf-8") as js:
+    with open(source_file, encoding="utf-8") as js:
         source_json = json.load(js)
         source_json.update(common_data())
         with open(cli_args.target, mode="w", encoding="utf-8") as target_file:
